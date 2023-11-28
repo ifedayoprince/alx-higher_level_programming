35f6712f198513636e2431e2fc181eedcdd52c1f
ifedayoprince <ifedayoprince@gmail.com>
2023-11-28 20:07:46 +0100

    feat: initial commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is a loop in a list
+ * @list: the pointer to the start of the list
+ * Return: 1 if there is cycle
+ *          Otherwise: 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list, *slow = list;
+
+	while (1)
+	{
+		if (fast == NULL)
+			return 0;
+
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow)
+			return 1;
+	}
+}
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "The Zen of Python, by Tim Peters"
+print(str)
