f1734fe4af80fab9724d22928729296b306cc03d
ifedayoprince <ifedayoprince@gmail.com>
2023-12-06 10:47:52 +0100

    fix: remove the test code

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,10 +4,3 @@ def delete_at(my_list=[], idx=0):
         return my_list
     my_list = my_list[:idx] + my_list[idx+1:]
     return my_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
-
