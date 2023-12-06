a7b4690232c79231b421ae6cd957d6ae2dcbe800
ifedayoprince <ifedayoprince@gmail.com>
2023-12-06 08:57:57 +0100

    fix: print number using the number format

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for item in my_list:
-        print("{}".format(item))
+        print("{:d}".format(item))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for i in range(1, len(my_list) + 1):
-        print("{}".format(my_list[-i]))
+        print("{:d}".format(my_list[-i]))
