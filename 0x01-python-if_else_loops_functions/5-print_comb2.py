d0c505a48eb2751de2598e6b070f95f1cb2605bf
ifedayoprince <ifedayoprince@gmail.com>
2023-12-29 11:36:25 +0100

    fix: add print_comb3 and refactored print statements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{}{}".format(str(i).zfill(2), ", " if i != 99 else ""), end="")
-print("".format())
+    print("{}".format(str(i).zfill(2)), end= ", " if i != 99 else "\n")
