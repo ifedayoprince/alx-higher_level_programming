5ed4405331091de8ae04d1de6112a3c1a2b03902
ifedayoprince <ifedayoprince@gmail.com>
2023-12-29 07:54:32 +0100

    fix: rewrite print_comp

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("{}".format(list(range(100))), sep=", ")
+
+for i in range(100):
+    print("{}{}".format(str(i).zfill(2), ", " if i != 99 else ""), end="")
+print("".format())
