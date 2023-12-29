5ab6271e330683666c9d3100e55d2ba05d389bc0
ifedayoprince <ifedayoprince@gmail.com>
2023-12-29 07:41:22 +0100

    fix: add case to check if number is negative and negate the last digit accordingly

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = int(number.__str__()[-1])
 
+if(number < 0):
+    last_digit *= -1
+
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} " \
+    print(f"Last digit of {number} is {last_digit} "
           "and is less than 6 and not 0")
