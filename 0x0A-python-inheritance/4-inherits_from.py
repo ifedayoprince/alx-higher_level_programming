b6b9eb488152c1aecf072ad5d015fb718c87c198
ifedayoprince <ifedayoprince@gmail.com>
2024-01-14 22:05:19 +0100

    fix: remove test code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -21,12 +21,3 @@ def inherits_from(obj, a_class):
     """
 
     return issubclass(type(obj), a_class)
-
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
