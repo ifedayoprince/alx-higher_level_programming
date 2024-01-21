5b2e98eb46bda4902071323f38be444f0d1ee99a
ifedayoprince <ifedayoprince@gmail.com>
2024-01-21 12:52:37 +0100

    feat: answer 'everything is object' project

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+The module containing code for:
+    copy_list
+"""
+
+
+def copy_list(l):
+    """
+    Copies a list and returns it.
+
+    Args:
+        l (list): The list.
+
+    Return:
+        list: A copy of the list.
+    """
+
+    return l.copy()
